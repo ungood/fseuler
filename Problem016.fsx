79ed5193bf8e494119c0610cb45d4d99dc1f6474
Jason Walker <ungood@onetrue.name>
2013-05-27 06:46:04 -0700

    Problem 16

---
+++ Problem016.fsx
@@ -0,0 +1,17 @@
+﻿// 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+// What is the sum of the digits of the number 2^1000?
+
+let Zero = bigint.Zero
+
+// Finds the digits of n, in base b
+let rec digits(n:bigint, b:bigint) =
+    if n.IsZero
+    then []
+    else
+        let (q,r) = bigint.DivRem(n,b)
+        r :: digits(q,b)
+        
+let x = bigint.Pow(bigint 2, 1000)
+
+digits(x, bigint 10)
+|> List.sum
