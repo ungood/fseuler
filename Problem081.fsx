750c7cdfd7918820f88a7daf76f4d130a2fd94de
Jason Walker <ungood@onetrue.name>
2013-05-28 19:16:14 -0700

    Problem 81

---
+++ Problem081.fsx
@@ -0,0 +1,28 @@
+﻿let example = array2D [
+    [ 131; 673; 234; 103;  18 ];
+    [ 201;  96; 342; 965; 150 ];
+    [ 630; 803; 746; 422; 111 ];
+    [ 537; 699; 497; 121; 956 ];
+    [ 805; 732; 524;  37; 331 ];
+]
+
+let solve(a:int[,]) =
+    let width = a.GetLength(1)
+    let height = a.GetLength(0)
+    let getVal(x, y) = if x >= width || y >= height then None else Some(a.[y,x])
+    let score(x, y) =
+        match (x,y) with
+        | (x,y) when x < width-1 && y < height-1 -> a.[y,x] + min a.[y+1,x] a.[y,x+1]
+        | (x,y) when x < width-1 -> a.[y,x] + a.[y,x+1]
+        | (x,y) when y < height-1 -> a.[y,x] + a.[y+1,x]
+        | (x,y) -> a.[y,x]
+    for y in (height-1).. -1 ..0 do
+        for x in (width-1).. -1 ..0 do
+            a.[y,x] <- score(x,y)
+    a.[0,0]
+
+let example_solution = solve(example)
+
+#load "Problem081_Data.fs"
+
+let solution = solve(Problem081_Data.problem_data)
